3ff53d205ffce24f3da15f4efa64a0502b23b1de
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-12-10 18:07:36 +0100

    Work session
    A huge work session with loads of work.

---
+++ assets/js/modal.js
@@ -0,0 +1,18 @@
+var modal = document.getElementById("contactModal");
+var btn = document.getElementById("btn");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
