bb58e881be0509d65f0d88ab7894bdb4635193eb
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-12-13 22:42:09 +0100

    teesting

--- assets/js/dropdown-test.js
+++ assets/js/dropdown-test.js
@@ -1,3 +1,27 @@
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.from(dropdown);
+
+dropdownArray.forEach(function(el){
+    el.onclick = function(event) {
+        var toggle = el.querySelector('a[data-toggle="dropdown"]'),
+            menu = el.querySelector('.dropdown-menu')
+
+        if(!menu.hasClass('dropdown')) {
+            menu.classList.add('dropdown-menu');
+            menu.classList.remove('hidden');
+            event.preventDefault();
+        }
+        else {
+            menu.classList.remove('dropdown-menu');
+            menu.classList.add('hidden');
+            event.preventDefault();
+        }
+    };
+})
+
+
+
+
 // Dropdown Menu
 var dropdown = document.querySelectorAll('.dropdown');
 var dropdownArray = Array.prototype.slice.call(dropdown,0);
@@ -22,5 +46,3 @@ dropdownArray.forEach(function(el){
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
-
-document.body.onclick = console.log
