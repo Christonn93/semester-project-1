ce996088d7f4422d893a1f4fc3b79dcd7401ed90
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-12-15 11:43:42 +0100

    Summary
    Adding the new layout to all pages.
    Now i need to fill in the content.
    
    The nav menu is working, both on mobile and desktop. Dropdown is designed correctly and tested on multipe platforms.

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,15 +1,22 @@
 const dropdownTrigger = document.querySelectorAll('a[data-toggle="dropdown"]');
 
 dropdownTrigger.forEach(function(trigger){
-	trigger.onclick = function(event){
-		const dropdownOpen = event.target.nextElementSibling;
-			if (dropdownOpen.style.display === "block"){
-				dropdownOpen.style.display = "none";
-				return;			
-			} else {
-				dropdownOpen.style.display = "none";
-			}{
-				event.target.nextElementSibling.style.display = "block";
-			}
-		}	
-	})
+    trigger.onclick = function(event){
+        const dropdownOpen = event.target.nextElementSibling;
+            if (dropdownOpen.style.display === "block"){
+                dropdownOpen.style.display = "none";
+                return;            
+            } else {
+                dropdownOpen.display = "none";
+            }{
+                event.target.nextElementSibling.style.display = "block";
+            }
+        }    
+    })
+
+	window.onclick = function(event) {
+		if (event.target == dropdown-menu) {
+		  dropdownOpen.style.display = "none";
+		}
+	  }
+	
