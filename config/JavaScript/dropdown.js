39f65845f2fcf6ca07fab6c06373785f618194fc
Christopher Tønnesland <christopher.tonnesland@gmail.com>
2021-12-19 19:51:08 +0100

    CSM v.3.0.1
    Changing out he hold pages with new ones

---
+++ config/JavaScript/dropdown.js
@@ -0,0 +1,13 @@
+
+const dropdownTrigger = document.querySelectorAll('a[data-toggle="dropdown"]');
+
+dropdownTrigger.forEach(function(trigger){
+    trigger.onclick = function(event){
+        const dropdownOpen = event.target.nextElementSibling;
+            if (dropdownOpen.style.display === "flex"){
+                dropdownOpen.style.display = "none";
+                return;            
+            }
+        }    
+    })
+
